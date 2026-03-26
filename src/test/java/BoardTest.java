7e578e44bbd95b11d4d30a990340dd39061c47a9
SeungGyu <phillipskl123@gmail.com>
2026-03-26 13:50:11 +0900

    test: Board 초기화 테스트 코드 구현

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,47 @@
+import domain.*;
+import domain.piece.Elephant;
+import domain.piece.Horse;
+import domain.piece.Piece;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BoardTest {
+
+    @DisplayName("초나라 포진을 상마마상으로 배치한다.")
+    @Test
+    void 초나라_포진을_상마마상으로_배치한다() {
+        Board board = new Board(Formation.from("1"), Formation.from("2"));
+        Map<Position, Piece> pieces = board.getBoard();
+
+        assertThat(pieces.get(Position.of(2, 10))).isInstanceOf(Elephant.class);
+        assertThat(pieces.get(Position.of(3, 10))).isInstanceOf(Horse.class);
+        assertThat(pieces.get(Position.of(7, 10))).isInstanceOf(Horse.class);
+        assertThat(pieces.get(Position.of(8, 10))).isInstanceOf(Elephant.class);
+    }
+
+    @DisplayName("한나라 포진을 마상마상으로 배치한다.")
+    @Test
+    void 한나라_포진을_마상마상으로_배치한다() {
+        Board board = new Board(Formation.from("1"), Formation.from("4"));
+        Map<Position, Piece> pieces = board.getBoard();
+
+        assertThat(pieces.get(Position.of(2, 1))).isInstanceOf(Horse.class);
+        assertThat(pieces.get(Position.of(3, 1))).isInstanceOf(Elephant.class);
+        assertThat(pieces.get(Position.of(7, 1))).isInstanceOf(Horse.class);
+        assertThat(pieces.get(Position.of(8, 1))).isInstanceOf(Elephant.class);
+    }
+
+    @DisplayName("각 궁이 올바른 진영에 배치된다.")
+    @Test
+    void 각_궁이_올바른_진영에_배치된다() {
+        Board board = new Board(Formation.from("1"), Formation.from("2"));
+        Map<Position, Piece> pieces = board.getBoard();
+
+        assertThat(pieces.get(Position.of(2, 5)).isSameSide(Side.HAN)).isTrue();
+        assertThat(pieces.get(Position.of(9, 5)).isSameSide(Side.CHO)).isTrue();
+    }
+}
