903920fd7bc8fc2af2e2ef0f78fb8ea496054b22
SeungGyu <phillipskl123@gmail.com>
2026-03-26 13:50:28 +0900

    test: Position 테스트 코드 구현

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,25 @@
+import domain.Position;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class PositionTest {
+
+    @Nested
+    class FromTest {
+
+        @ParameterizedTest
+        @CsvSource({
+                "0, 10",
+                "1, 11"
+        })
+        void 목적지_좌표가_가로_1_9_세로_1_10을_벗어나면_예외가_발생한다(int x, int y) {
+            // when & then
+            Assertions.assertThatThrownBy(() -> Position.of(x, y))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("y 좌표는 1~10, x 좌표는 1~9 사이여야 합니다.");
+        }
+    }
+}
